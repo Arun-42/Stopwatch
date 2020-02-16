2d68db4b68edb2ea3d96001d9c8c7596eb90b29b
T Arun <arunthulasiraman@gmail.com>
2020-02-16 22:47:04 +0530

    Using the Date module instead now.
    Millisecond works too! Also inverted the logs.

--- README.md
+++ README.md
@@ -4,8 +4,3 @@ It's a very basic single-page web app. To get familiar with React.
 
 Link to the gh-page: https://arun-42.github.io/Stopwatch/.
 
-ToDo
-Add some kind of background when the stopwatch is running
-Invert the redering of log
-Use the Date Library instead of adding manully every second
-Add millisecond
--- src/index.js
+++ src/index.js
@@ -12,18 +12,20 @@ class Top extends React.Component {
     this.setms = null;
     this.state = {
       started: false,
-      h: 0,
-      m: 0,
-      s: 0,
+      time: 0,
       log: [],
     };
   }
+  msToTime(s) {
+    // Pad to 2 or 3 digits, default is 2
+    return new Date(s).toISOString().slice(11,-1);
+  }
+
   log(){
-    const timeArr = [this.getH(),this.getM(),this.getS()];
-    let log_state = this.state.log;
-    if (log_state.indexOf(timeArr)>0) {return;}
     if (!this.state.started){return}
-    log_state.push(timeArr); 
+    const timeArr = this.state.time;
+    let log_state = this.state.log;
+    log_state.push(timeArr);
     this.setState({
       ...this.state,
       log : log_state,
@@ -34,70 +36,34 @@ class Top extends React.Component {
     this.setState({
       ...this.state,
       started: true,
-    })    
-    this.seth = setInterval(() => {
-      this.setState({
-        ...this.state,
-        h: 1 + this.state.h,
-      });
-    }, 1000 * 60 * 60);
-    this.setm = setInterval(() => {
-      this.setState({
-        ...this.state,
-        m: 1 + this.state.m
-      });
-    }, 1000 * 60);
+    });
+    let current = this.state.time;
+    let start = new Date() - current;
     this.sets = setInterval(() => {
       this.setState({
         ...this.state,
-        s: 1 + this.state.s
+        time : (new Date() - start),
       });
-      if (this.state.s >= 60) {
-        this.setState({
-          ...this.state,
-          s: 0
-        });
-      }
-      if (this.state.m >= 60) {
-        this.setState({
-          ...this.state,
-          m: 0
-        });
-      }
-    }, 1000);
+    }, 1);
   }
   clearIntervals() {
     this.setState({
       ...this.state,
       started: false,
     })
-    clearInterval(this.seth);
-    clearInterval(this.setm);
     clearInterval(this.sets);
   }
   resetAll() {
     this.setState({
-      h: 0,
-      m: 0,
-      s: 0,
       log: [],
+      time: 0,
     }, () => {this.clearIntervals();});
-  }
-  
-  getS() {
-    return this.state.s < 10 ? "0" + String(this.state.s) : String(this.state.s);
-  }
-  getM() {
-    return this.state.m < 10 ? "0" + String(this.state.m) : String(this.state.m);
-  }
-  getH() {
-    return this.state.h < 10 ? "0" + String(this.state.h) : String(this.state.h);
   }
   makeLogs(){
     let elem = [];
     this.state.log.forEach(element => {
       elem.push(
-        <h3 key={this.state.log.indexOf(element)}>{element[0]}:{element[1]}:{element[2]}</h3>
+        <h3 key={this.state.log.indexOf(element)}>{this.msToTime(element)}</h3>
       )
     });
     return elem;
@@ -127,11 +93,11 @@ class Top extends React.Component {
         <h3>Hours : Minutes : Seconds</h3>
         <div className="clock">
           <h3>
-            {this.getH.bind(this)()}:{this.getM.bind(this)()}:
-            {this.getS.bind(this)()}
+            {this.msToTime(this.state.time)}
           </h3>
         </div>
-        <div className="logs">{logs}</div>
+        <div className="logs">{logs.reverse()}</div>
+
       </div>
     );
   }
