6418666f385db34559ae28b6c8f74b9caa0bd0b5
T Arun <arunthulasiraman@gmail.com>
2020-02-21 23:48:30 +0530

    Refactored to add a Buttons component.
    The render got too clumpsy so created a buttons component. Added background to the clock.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+require("./index.css");
+
+// this is super clumpsy on second thought
+
+function start_stop(started) {
+  return started ? "Stop" : "Start";
+}
+
+function start_stoplog(started) {
+  return started ? "Lap" : "Reset";
+}
+
+function Buttons(props) {
+  const started = props.started;
+  return (
+    <div className="buttons">
+      <button onClick={props.start_clear}>{start_stop(started)}</button>
+      <button onClick={props.start_clearlog}>{start_stoplog(started)}</button>
+    </div>
+  );
+}
 
 class Top extends React.Component {
   constructor(props) {
     super(props);
     this.sets = null;
+    this.start_clear = this.start_clear.bind(this);
+    this.start_clearlog = this.start_clearlog.bind(this);
     this.state = {
       started: false,
       time: 0,
       log: []
     };
   }
+
   msToTime(s) {
     // Pad to 2 or 3 digits, default is 2
     return new Date(s).toISOString().slice(11, -1);
@@ -25,30 +48,27 @@ class Top extends React.Component {
     let log_state = this.state.log;
     log_state.push(timeArr);
     this.setState({
-      ...this.state,
       log: log_state
     });
   }
+
   start() {
     if (this.state.started) {
       return;
     }
     this.setState({
-      ...this.state,
       started: true
     });
     let current = this.state.time;
     let start = new Date() - current;
     this.sets = setInterval(() => {
       this.setState({
-        ...this.state,
         time: new Date() - start
       });
     }, 1);
   }
   clearIntervals() {
     this.setState({
-      ...this.state,
       started: false
     });
     clearInterval(this.sets);
@@ -65,16 +85,15 @@ class Top extends React.Component {
     );
   }
   makeLogs() {
-    let elem = [];
-    this.state.log.forEach(element => {
+    let elem = this.state.log.map(element => (
+      <h3 key={this.state.log.indexOf(element)}>{this.msToTime(element)}</h3>
+    ));
+    /* this.state.log.forEach(element => {
       elem.push(
         <h3 key={this.state.log.indexOf(element)}>{this.msToTime(element)}</h3>
       );
-    });
+    }); */
     return elem;
-  }
-  start_stop() {
-    return this.state.started ? "Stop" : "Start";
   }
   start_clear() {
     if (this.state.started) {
@@ -90,27 +109,29 @@ class Top extends React.Component {
       this.resetAll.apply(this);
     }
   }
-  start_stoplog() {
-    return this.state.started ? "Log" : "Reset";
-  }
   render() {
     let logs = this.makeLogs.apply(this);
+    //const started = this.state.started;
+    const clockstyle = {
+      background: this.state.started
+        ? "rgba(68, 64, 194, 0.2)"
+        : this.state.time
+        ? "rgba(226,44,100,0.4)"
+        : "none"
+    };
     return (
       <div className="main">
         <h2>
           Stopwatch {/* <span style={{fontSize:0.4+'em'}}>kindof</span> */}
         </h2>
-        <div className="buttons">
-          <button onClick={this.start_clear.bind(this)}>
-            {this.start_stop.apply(this)}
-          </button>
-          <button onClick={this.start_clearlog.bind(this)}>
-            {this.start_stoplog.apply(this)}
-          </button>
-        </div>
+        <Buttons
+          started={this.state.started}
+          start_clear={this.start_clear}
+          start_clearlog={this.start_clearlog}
+        />
         <br></br>
-        <h3>Hours : Minutes : Seconds</h3>
-        <div className="clock">
+        {/* <h3>Hours : Minutes : Seconds</h3> */}
+        <div className="clock" style={clockstyle}>
           <h3>{this.msToTime(this.state.time)}</h3>
         </div>
         <div className="logs">{logs.reverse()}</div>
