e8e9f7cd754eb19f998d36239b3e8d6bee183645
T Arun <arunthulasiraman@gmail.com>
2020-03-05 01:47:31 +0530

    Refactored to use Hooks
    A lot of room for improvement. The code is truly much cleaner to write and read with hooks.
    Using a custom hook this code can be made super clean.

--- src/index.js
+++ src/index.js
@@ -1,72 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
-import {Heading} from "./components/heading"
-import  {Laps} from "./components/laps"
-import {Clock, msToTime} from "./components/clock"
-import {Buttons} from "./components/buttons"
+import "./index.css";
+import { Heading } from "./components/heading";
+import { Laps } from "./components/laps";
+import { Clock, msToTime } from "./components/clock";
+import { Buttons } from "./components/buttons";
 
 
-class Top extends React.Component {
-  constructor(props) {
-    super(props);
-    this.sets = null;
-    this.start_clear = this.start_clear.bind(this);
-    this.start_clearlap = this.start_clearlap.bind(this);
-    this.state = {
-      started: false,
-      time: 0,
-      lap: []
-    };
-  }
+let sets = null;
 
-  lap() {
-    if (!this.state.started) {
+function Top() {
+
+  const [state, setState] = useState({
+    started: false,
+    time: 0,
+    lap: []
+  });
+
+  useEffect(() => {
+    if (!state.started) {
+      clearInterval(sets);
+    }
+  }, [state.started]);
+
+  function lap() {
+    if (!state.started) {
       return;
     }
-    const timeArr = this.state.time;
-    let lap_state = this.state.lap;
+    const timeArr = state.time;
+    let lap_state = state.lap;
     lap_state.push(timeArr);
-    this.setState({
+    setState({
+      ...state,
       lap: lap_state
     });
   }
 
-  start() {
-    if (this.state.started) {
+  function start() {
+    if (state.started) {
       return;
     }
-    this.setState({
+    setState({
+      ...state,
       started: true
     });
-    let current = this.state.time;
+    let current = state.time;
     let start = new Date() - current;
-    this.sets = setInterval(() => {
-      this.setState({
+    sets = setInterval(() => {
+      setState({
+        ...state,
+        started: true,
         time: new Date() - start
       });
     }, 1);
   }
-  clearIntervals() {
-    this.setState({
+
+  function clearIntervals() {
+    setState({
+      ...state,
       started: false
     });
-    clearInterval(this.sets);
+    //    clearInterval(sets);
   }
-  resetAll() {
-    this.setState(
-      {
-        lap: [],
-        time: 0
-      },
-      () => {
-        this.clearIntervals();
-      }
-    );
+
+  function resetAll() {
+    setState({
+      ...state,
+      lap: [],
+      time: 0
+    });
+    //fix this. this has to be run.
+    //maybe we should use useEffect for setInterval
+    //() => {this.clearIntervals();}
   }
-  makelaps() {
-    let elem = this.state.lap.map(element => (
-      <h3 key={this.state.lap.indexOf(element)}>{msToTime(element)}</h3>
+
+  function makelaps() {
+    let elem = state.lap.map(element => (
+      <h3 key={state.lap.indexOf(element)}>{msToTime(element)}</h3>
     ));
     /* this.state.lap.forEach(element => {
       elem.push(
@@ -75,44 +85,50 @@ class Top extends React.Component {
     }); */
     return elem;
   }
-  start_clear() {
-    if (this.state.started) {
-      this.clearIntervals.apply(this);
-    } else {
-      this.start.apply(this);
-    }
-  }
-  start_clearlap() {
-    if (this.state.started) {
-      this.lap.apply(this);
-    } else {
-      this.resetAll.apply(this);
-    }
-  }
-  render() {
-    const laps = this.makelaps.apply(this);
-    //const started = this.state.started;
 
-    return (
-      <div className="main">
-        <div className="head">
-          <Heading />
-          <Buttons
-            started={this.state.started}
-            start_clear={this.start_clear}
-            start_clearlap={this.start_clearlap}
-          />
-          <Clock 
-            time={this.state.time} 
-            started={this.state.started} 
-          />
-        </div>
-        <Laps laps={laps} />
-      </div>
-    );
+  function start_clear() {
+    if (state.started) {
+      clearIntervals();
+    } else {
+      start();
+    }
   }
+
+  function start_clearlap() {
+    if (state.started) {
+      lap();
+    } else {
+      resetAll();
+    }
+  }
+  const laps = makelaps();
+  return (
+    <div className="main">
+      <div className="head">
+        <Heading />
+        <Buttons
+          started={state.started}
+          start_clear={start_clear}
+          start_clearlap={start_clearlap}
+        />
+        <Clock time={state.time} started={state.started} />
+      </div>
+      <Laps laps={laps} />
+    </div>
+  );
 }
 
-
-
 ReactDOM.render(<Top />, document.getElementById("root"));
+
+//ReactDOM.render(<Hello />, document.getElementById("roo"));
+/* function Hello(){
+      const [count,setCount] = useState(0);
+      return(
+        <div>
+          <button onClick={()=>setCount(count+1)}>
+            clickk
+          </button>
+      <h3>count:{count}</h3>
+        </div>
+      )
+    } */
